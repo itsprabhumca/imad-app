3db7a5d662f97bc3811a67d4f6239cdf96514ae5
Prabhu E <31686277+itsprabhumca@users.noreply.github.com>
2017-09-12 19:27:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,61 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+    title : 'Article - One | Prabhu',
+    heading : 'Article - One',
+    date : 'Sep 12, 2017'
+    content : "
+              <br/>
+              <br/>
+              <b>About Hasura</b>
+              <p align='justify'>
+                Hasura is a batteries included platform for building and deploying powerful backends. Instant APIs for data, auth & file handling (powered by Postgres). Hasura helps you deploy your custom code written in any language/framework. Runs anywhere: your laptop, your VM, your cluster on the cloud.
+              </p>"
+};
+
+function createTemplate(Data) {
+    
+    var title = Data.title;
+    var header = Data.header;
+    var content = Data.content;
+    
+    var htmlTemplate = '
+          <html>
+          <head>
+              <title> $(title);</title>
+              <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+              <link href="/ui/style.css" rel="stylesheet" />
+          </head>
+          <body>
+              <div class='container'>
+                  <a href="/"><h5>Goto Home</h5></a>
+                  <hr>
+                  <div>
+                      <h3>$(header);</h3>
+                      
+                  </div>
+                  <br/>
+                  <div>
+                    <b>Today : </b> $(date);
+                  </div>
+                  <div>
+                        $(content);   
+                  </div>
+              </div>
+          </body>
+        </html>  
+    ';
+    
+    return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',function(req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   res.sendFile(articleOne);
 });
 
 app.get('/article-two',function(req,res){
