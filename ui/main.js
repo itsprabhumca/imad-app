91c87bcf3d6a11a9d81a48da86e8f96ada74ef26
itsprabhumca <itsprabhumca@gmail.com>
2017-09-14 19:35:52 +0530

    HTTP Request Modified

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,20 @@
 console.log('Loaded!');
-var cnt = 0;
+// Counter Code
 var button = document.getElementById('btnCounter');
 button.onclick = function() {
-    cnt += 1;
-    var count = document.getElementById('count');
-    count.innerHTML = cnt;
+    // Make a request to Counter EndPoint
+    var request = new XMLHttpRequest();
+
+    request.onreadystatechange = function() {
+        if(request.readyState == XMLHttpRequest.DONE) {
+            if(request.status == 200) {
+                var cnt = request.responseText;
+                var count = document.getElementById('count');
+                count.innerHTML = cnt;
+            }
+        }
+    };
+
+    request.open('GET','http://itsprabhumca.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
